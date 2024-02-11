43eefd4f13574d4dd03f6365411cf51fea3e87f2
AP <arjavprasad@gmail.com>
2024-02-11 13:07:02 -0800

    code polish

--- accel_and_tof/accel_and_tof.ino
+++ accel_and_tof/accel_and_tof.ino
@@ -23,28 +23,243 @@ Adafruit_VL6180X lox3 = Adafruit_VL6180X();
 Adafruit_VL6180X lox4 = Adafruit_VL6180X();
 
 // Setup mode for doing reads
-typedef enum {RUN_MODE_DEFAULT} runmode_t;
+typedef enum { RUN_MODE_DEFAULT } runmode_t;
 runmode_t run_mode = RUN_MODE_DEFAULT;
 
-// ADDED - MINIMUM SAFE DISTANCE (mm)
-const int MIN_DIST = 50;
-// ADDED - MAXIMUM SAFE DISTANCE (mm)
-const int MAX_DIST = 100;
-const int XmaxExtension = 190;
-const int YmaxExtension = 100;
-// ADDED - DISTANCE OF THE FIRST SENSOR (mm)
-int ONE_DIST;
-// ADDED - DISTANCE OF THE SECOND SENSOR (mm)
-int TWO_DIST;
-// ADDED - DISTANCE OF THE THIRD SENSOR (mm)
-int THREE_DIST;
-// ADDED - DISTANCE OF THE REAR SENSOR (mm)
-int REAR_DIST = 1000;
-// REAR DISTANCE TO DETECT CRASH
-const int safetyDistance = 150;
+//<------------CONSTANTS------------>
+// MIN/MAX SAFE DISTANCE (mm)
+const int MIN_SAFE_DIST = 50;
+const int MAX_SAFE_DIST = 100;
 
+// MAX Extension DISTANCE (mm)
+const int X_MAX_EXTENSION = 190;
+const int Y_MAX_EXTENSION = 100;
+
+// MIN ACCELERATION VALUE TO DETECT CRASH
+const int SAFETY_ACCEL = 0;
+
+// MIN SAFETY VALUE FOR REAR DISTANCE TO DETECT CRASH
+const int REAR_SAFETY_DISTANCE = 150;
+
+//<------------GLOBAL VARIABLES------------>
+// DISTANCE OF THE FIRST SENSOR (mm)
+int topSensorDist;
+// DISTANCE OF THE SECOND SENSOR (mm)
+int middleSensorDist;
+// DISTANCE OF THE THIRD SENSOR (mm)
+int bottomSensorDist;
+
+// DISTANCE OF THE REAR SENSOR (mm) - Default set to 1000 to avoid error
+int rearObjDist = 1000;
+
+// ACCELERATION (m/s^2)
 int accel;
-const int safetyAccel = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  while (!Serial) {
+    delay(1);
+  }
+
+  pinMode(SHT_LOX1, OUTPUT);
+  pinMode(SHT_LOX2, OUTPUT);
+  pinMode(SHT_LOX3, OUTPUT);
+  pinMode(SHT_LOX4, OUTPUT);
+
+  Serial.println("Shutdown pins inited...");
+
+  digitalWrite(SHT_LOX1, LOW);
+  digitalWrite(SHT_LOX2, LOW);
+  digitalWrite(SHT_LOX3, LOW);
+  digitalWrite(SHT_LOX4, LOW);
+  Serial.println("All in reset mode...(pins are low)");
+
+  Serial.println("Starting...");
+  setID();
+}
+
+void loop() {
+  Serial.println("<----------START SENSOR READING---------->");
+  Serial.println("<--Checking for Crash 😬-->");
+  readLidarSensor(lox4);
+  if (rearObjDist <= REAR_SAFETY_DISTANCE) {
+    readMPU6050();
+    if (accel >= SAFETY_ACCEL) {
+      Serial.println("<--CRASH DETECTED!! 😱-->");
+      Serial.println();
+      senseHeadPosition();
+      processHeadPosition();
+    }
+  }
+  delay(10);
+}
+
+void senseHeadPosition() {
+  Serial.println("<--Sensing Head Position 👀-->");
+  readLidarSensor(lox1);
+  readLidarSensor(lox2);
+  readLidarSensor(lox3);
+  Serial.println();
+}
+
+void readMPU6050() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  accel = a.acceleration.x;
+
+  Serial.print("Accel X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  // Serial.print("Rotation X: ");
+  // Serial.print(g.gyro.x);
+  // Serial.print(", Y: ");
+  // Serial.print(g.gyro.y);
+  // Serial.print(", Z: ");
+  // Serial.print(g.gyro.z);
+  // Serial.println(" rad/s");
+
+  // Serial.print("Temperature: ");
+  // Serial.print(temp.temperature);
+  // Serial.println(" degC");
+
+  Serial.println("");
+}
+
+void readLidarSensor(Adafruit_VL6180X &vl) {
+  // Serial.print(" Addr: ");
+  // Serial.print(vl.getAddress(), HEX);
+
+  float lux = vl.readLux(VL6180X_ALS_GAIN_5);
+  //Serial.print(" Lux: "); Serial.print(lux);
+  uint8_t range = vl.readRange();
+  uint8_t status = vl.readRangeStatus();
+
+  //assign the distances their value
+  if (status == VL6180X_ERROR_NONE) {
+    if (vl.getAddress() == 0x30) {
+      Serial.print("Top Sensor: ");
+      Serial.print(range);
+      Serial.println(" mm");
+      topSensorDist = range;
+    }
+    if (vl.getAddress() == 0x31) {
+      Serial.print("Middle Sensor: ");
+      Serial.print(range);
+      Serial.println(" mm");
+      middleSensorDist = range;
+    }
+    if (vl.getAddress() == 0x32) {
+      Serial.print("Bottom Sensor: ");
+      Serial.print(range);
+      Serial.println(" mm");
+      bottomSensorDist = range;
+    }
+    if (vl.getAddress() == 0x33) {
+      Serial.print("Rear Sensor: ");
+      Serial.print(range);
+      Serial.println(" mm");
+      rearObjDist = range;
+    }
+  }
+
+  //if top sensor detects nothing, give it a distinct value
+  else if (vl.getAddress() == 0x30) {
+    topSensorDist = 1000;
+    Serial.print("Top Sensor: NO HEAD DETECTED");
+    Serial.println();
+  }
+
+  //if middle sensor detects nothing, give it a distinct value
+  else if (vl.getAddress() == 0x31) {
+    middleSensorDist = 1000;
+    Serial.print("Middle Sensor: NO HEAD DETECTED");
+    Serial.println();
+  }
+
+  //if bottom sensor detects nothing, give it a distinct value
+  else if (vl.getAddress() == 0x32) {
+    bottomSensorDist = 1000;
+    Serial.print("Bottom Sensor: NO HEAD DETECTED");
+    Serial.println();
+  }
+
+  //if rear sensor detects nothing, give it a distinct value
+  else if (vl.getAddress() == 0x33) {
+    rearObjDist = 1000;
+    Serial.print("Rear Sensor: NO OBJECT DETECTED");
+    Serial.println();
+  }
+}
+
+void processHeadPosition() {
+  Serial.println("<--Process Head Position 🤔-->");
+  //top sensor detects nothing
+  if (topSensorDist == 1000) {
+
+    //middle sensor detects nothing
+    if (middleSensorDist == 1000) {
+
+      //bottom sensor detects nothing
+      if (bottomSensorDist == 1000) {
+        Serial.print("X EXTEND: ");
+        Serial.print(X_MAX_EXTENSION);
+        Serial.println(" mm");
+        Serial.println();
+      }
+
+      //bottom sensor detects a head
+      else {
+        Serial.print("Y RETRACT ");
+        Serial.print(Y_MAX_EXTENSION);
+        Serial.println(" mm");
+        Serial.println();
+      }
+    }
+
+    //the height is good
+    else {
+      Serial.print("CORRECT HEIGHT");
+      Serial.println();
+
+      //the middle sensor is in range
+      if (middleSensorDist > MIN_SAFE_DIST && middleSensorDist < MAX_SAFE_DIST) {
+        Serial.print("CORRECT DISTANCE");
+        Serial.println();
+      }
+
+      //the middle sensor is too close
+      else if (middleSensorDist < MIN_SAFE_DIST) {
+        Serial.print("X RETRACT ");
+        Serial.print(MIN_SAFE_DIST - middleSensorDist);
+        Serial.println(" mm");
+        Serial.println();
+      }
+
+      //the middle sensor is too far
+      else {
+        Serial.print("X EXTEND ");
+        Serial.print(middleSensorDist - MAX_SAFE_DIST);
+        Serial.println(" mm");
+        Serial.println();
+      }
+    }
+  }
+
+  //top sensor detects a head
+  else if (topSensorDist != 1000) {
+    Serial.print("Y EXTEND ");
+    Serial.print(Y_MAX_EXTENSION / 2);
+    Serial.println(" mm");
+    Serial.println();
+  }
+}
 
 
 void setID() {
@@ -87,7 +302,8 @@ void setID() {
   // initing LOX1
   if (!lox1.begin()) {
     Serial.println(F("Failed to boot first VL6180X"));
-    while (1);
+    while (1)
+      ;
   }
   lox1.setAddress(LOX1_ADDRESS);
   delay(10);
@@ -99,7 +315,8 @@ void setID() {
   //initing LOX2
   if (!lox2.begin()) {
     Serial.println(F("Failed to boot second VL6180X"));
-    while (1);
+    while (1)
+      ;
   }
   lox2.setAddress(LOX2_ADDRESS);
 
@@ -110,7 +327,8 @@ void setID() {
   //initing LOX3
   if (!lox3.begin()) {
     Serial.println(F("Failed to boot third VL6180X"));
-    while (1);
+    while (1)
+      ;
   }
   lox3.setAddress(LOX3_ADDRESS);
 
@@ -121,231 +339,9 @@ void setID() {
   //initing LOX4
   if (!lox4.begin()) {
     Serial.println(F("Failed to boot fourth VL6180X"));
-    while (1);
+    while (1)
+      ;
   }
   lox4.setAddress(LOX4_ADDRESS);
 }
 
-void setup() {
-  Serial.begin(115200);
-
-  while (!Serial) {
-    delay(1);
-  }
-
-  pinMode(SHT_LOX1, OUTPUT);
-  pinMode(SHT_LOX2, OUTPUT);
-  pinMode(SHT_LOX3, OUTPUT);
-  pinMode(SHT_LOX4, OUTPUT);
-
-  Serial.println("Shutdown pins inited...");
-
-  digitalWrite(SHT_LOX1, LOW);
-  digitalWrite(SHT_LOX2, LOW);
-  digitalWrite(SHT_LOX3, LOW);
-  digitalWrite(SHT_LOX4, LOW);
-  Serial.println("All in reset mode...(pins are low)");
-
-  Serial.println("Starting...");
-  setID();
-}
-
-void loop() {
-  readSensor(lox4);
-  if (REAR_DIST <= safetyDistance){
-    readMPU6050();
-    if (accel >= safetyAccel){
-      headPosition();
-      checkPosition();
-    }
-  }
-  delay(10);
-}
-
-void headPosition() {
-  readSensor(lox1);
-  readSensor(lox2);
-  readSensor(lox3);
-  Serial.println();
-}
-
-void readMPU6050() {
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-
-  accel = a.acceleration.x;
-
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-
-  // Serial.print("Rotation X: ");
-  // Serial.print(g.gyro.x);
-  // Serial.print(", Y: ");
-  // Serial.print(g.gyro.y);
-  // Serial.print(", Z: ");
-  // Serial.print(g.gyro.z);
-  // Serial.println(" rad/s");
-
-  // Serial.print("Temperature: ");
-  // Serial.print(temp.temperature);
-  // Serial.println(" degC");
-
-  Serial.println("");
-}
-
-void readSensor(Adafruit_VL6180X &vl) {
-  Serial.print(" Addr: ");
-  Serial.print(vl.getAddress(), HEX);
-
-  float lux = vl.readLux(VL6180X_ALS_GAIN_5);
-  //Serial.print(" Lux: "); Serial.print(lux);
-  uint8_t range = vl.readRange();
-  uint8_t status = vl.readRangeStatus();
-
-  //assign the distances their value
-  if (status == VL6180X_ERROR_NONE) {
-    Serial.print(" Range: "); Serial.print(range);
-    Serial.println();
-    if (vl.getAddress() == 0x30) {
-      ONE_DIST = range;
-    }
-    if (vl.getAddress() == 0x31) {
-      TWO_DIST = range;
-    }
-    if (vl.getAddress() == 0x32) {
-      THREE_DIST = range;
-    }
-    if (vl.getAddress() == 0x33) {
-      REAR_DIST = range;
-    }
-  }
-
-    //if top sensor detects nothing, give it a distinct value
-  else if (vl.getAddress() == 0x30) {
-    ONE_DIST = 1000;
-    Serial.print(" NO HEAD DETECTED");
-    Serial.println();
-  }
-
-  //if middle sensor detects nothing, give it a distinct value
-  else if (vl.getAddress() == 0x31) {
-    TWO_DIST = 1000;
-    Serial.print(" NO HEAD DETECTED");
-    Serial.println();
-  }
-
-  //if bottom sensor detects nothing, give it a distinct value
-  else if (vl.getAddress() == 0x32) {
-    THREE_DIST = 1000;
-    Serial.print(" NO HEAD DETECTED");
-    Serial.println();
-  }
-
-  //if rear sensor detects nothing, give it a distinct value
-  else if (vl.getAddress() == 0x33) {
-    REAR_DIST = 1000;
-    Serial.print(" NO OBJECT DETECTED");
-    Serial.println();
-  }
-
-}
-
-void checkPosition() {
-  
-  //top sensor detects nothing
-  if (ONE_DIST == 1000) {
-
-    //middle sensor detects nothing
-    if (TWO_DIST == 1000) {
-
-      //bottom sensor detects nothing
-      if (THREE_DIST == 1000) {
-        Serial.print("MOVE FORWARD ");
-        Serial.print(XmaxExtension);
-        Serial.println();
-      }
-
-      //bottom sensor detects a head
-      else {
-        Serial.print("Y RETRACT ");
-        Serial.print(YmaxExtension);
-        Serial.println();
-      }
-    }
-
-    //the height is good
-    else {
-      Serial.print("CORRECT HEIGHT");
-      Serial.println();
-
-      //the middle sensor is in range
-      if (TWO_DIST > MIN_DIST && TWO_DIST < MAX_DIST) {
-        Serial.print("CORRECT DISTANCE");
-
-//        //the bottom sensor is in range
-//        if (THREE_DIST > MIN_DIST && THREE_DIST < MAX_DIST) {
-//          Serial.print("CORRECT DISTANCE & TILT");
-//          Serial.println();
-//        }
-//
-//        //the bottom sensor is too close
-//        else if (THREE_DIST < MIN_DIST) {
-//          Serial.print("TILT FORWARD");
-//          Serial.println();
-//        }
-//
-//        //the bottom sensor is too far
-//        else {
-//          Serial.print("TILT BACK");
-//          Serial.println();
-//        }
-      }
-
-      //the middle sensor is too close
-      else if (TWO_DIST < MIN_DIST) {
-
-//        //the bottom sensor is too close
-//        if (THREE_DIST < MIN_DIST) {
-          Serial.print("X RETRACT ");
-          Serial.print(MIN_DIST - TWO_DIST);
-          Serial.println();
-//        }
-
-//        //the bottom sensor is in range
-//        else {
-//          Serial.print("TILT BACK");
-//          Serial.println();
-//        }
-      }
-
-      //the middle sensor is too far
-      else {
-
-//        //the bottom sensor is too far
-//        if (THREE_DIST > MAX_DIST) {
-          Serial.print("X EXTEND ");
-          Serial.print(TWO_DIST - MAX_DIST);
-          Serial.println();
-//        }
-
-//        //the bottom sensor is in range
-//        else {
-//          Serial.print("TILT FORWARD");
-//          Serial.println();
-//        }
-      }
-    }
-  }
-
-  //top sensor detects a head
-  else if (ONE_DIST != 1000) {
-    Serial.print("Y EXTEND ");
-    Serial.print(YmaxExtension/2);
-    Serial.println();
-  }
-}
